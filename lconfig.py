6d1f490ee770dfaddede86b9cc951c7c4a07f79a
Jan Höcker <jhoecker@posteo.de>
2016-09-29 16:12:49 +0200

    Move from dialog to message.

--- lconfig.py
+++ lconfig.py
@@ -3,29 +3,26 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class ConfDialog(QtGui.QDialog):
+class ConfDialog(QtGui.QMessageBox):
     def __init__(self, parent=None):
         QtGui.QDialog.__init__(self, parent)
         
         metaDataKeys = ['FOV', 'Start Voltage', 'Sample Temp.', 'Main',
                         'timestamp', 'test']
         
-        layout = QtGui.QGridLayout()
-        self.btn_ok = QtGui.QPushButton('Ok', self)
-        self.btn_cnl = QtGui.QPushButton('Cancel', self)
+        # Instantiate UI elements
+        innerLayoutCheckboxes = QtGui.QGridLayout()
         
         ii = 0
         for item in metaDataKeys:
             ## Use list with strings to create checkboxes
-            layout.addWidget(QtWidgets.QCheckBox(item), ii-ii%2,ii%2)
+            innerLayoutCheckboxes.addWidget(QtWidgets.QCheckBox(item), ii-ii%2,ii%2)
             ii += 1
-        #layout.setColumnStretch(ii, 1)
-        #layout.setRowStretch(ii,2)
-        layout.addWidget(self.btn_ok, ii, 1, 1, 1)
-        layout.addWidget(self.btn_cnl, ii, 2, 1, 1)
-        self.setLayout(layout)
+
+        self.layout().addLayout(innerLayoutCheckboxes, 0, 0)
         
-        self.setGeometry(300, 300, 290, 150)
+        self.setStandardButtons(self.Ok | self.Cancel)
+        #self.setGeometry(300, 300, 290, 150)
         self.setWindowTitle('Configuration')
         
         
